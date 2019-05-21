18c75327d65a2c64459ec4a5037345fa8d71ccf8
George Witteman <george@georgewitteman.com>
2019-05-20 20:49:20 -0700

    Fix filename

--- functions/gi.fish
+++ functions/gi.fish
@@ -1,5 +1,5 @@
 function gi -d "gitignore.io cli for fish"
-  if ! test -f $fish_complete_path[1]
+  if ! test -f ~/.config/fish/completions/gi.fish
     echo "Getting gi completions"
     __gi_update_completions
   else if test $argv[1] = 'update-completions'
