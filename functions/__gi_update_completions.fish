a700fa04d103519ebe7a29f23f0bca58a67a96a7
George Witteman <george@georgewitteman.com>
2019-05-20 20:24:50 -0700

    Don't include files in completions

--- functions/__gi_update_completions.fish
+++ functions/__gi_update_completions.fish
@@ -17,5 +17,5 @@ function __gi_update_completions -d "Update completions for gitignore.io"
   end
 
   # Output new completions
-  echo "complete -c gi -a \"update-completions $gi_list\"" >$compl_file
+  echo "complete --command gi --no-files --arguments \"update-completions $gi_list\"" >$compl_file
 end
