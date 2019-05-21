cf2c2da29388d5cca385c495074e4134d7091d1e
George Witteman <george@georgewitteman.com>
2019-05-20 20:52:22 -0700

    Better list

--- functions/gi.fish
+++ functions/gi.fish
@@ -5,6 +5,9 @@ function gi -d "gitignore.io cli for fish"
   else if test $argv[1] = 'update-completions'
     __gi_update_completions
     return $status
+  else if test $argv[1] = 'list'
+    curl -s https://www.gitignore.io/api/list | tr ',' \n
+    return $status
   end
 
   set -l params (echo $argv|tr ' ' ',')
