c1a63b643e7ea4979c31839c75ff4263bead446f
George Witteman <george@georgewitteman.com>
2019-05-20 20:31:29 -0700

    Automatically add completions if they don't exist

--- functions/gi.fish
+++ functions/gi.fish
@@ -1,5 +1,8 @@
 function gi -d "gitignore.io cli for fish"
-  if test $argv[1] = 'update-completions'
+  if ! test -f $fish_complete_path[1]
+    echo "Getting gi completions"
+    __gi_update_completions
+  else if test $argv[1] = 'update-completions'
     __gi_update_completions
     return $status
   end
