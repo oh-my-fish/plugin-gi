5f8f1952bc082525c83ac432fb97632fe78dc161
Levent Yalcin <leventyalcin@gmail.com>
2022-04-22 22:53:53 +0100

    follow redirects

--- functions/gi.fish
+++ functions/gi.fish
@@ -5,5 +5,5 @@ function gi -d "gitignore.io cli for fish"
   end
 
   set -l params (echo $argv|tr ' ' ',')
-  curl -s https://www.gitignore.io/api/$params
+  curl --silent --location https://www.gitignore.io/api/$params
 end
